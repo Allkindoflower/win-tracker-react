c816b8f96497be91baae33eb22f253cf12947494
Ugur Bastug <bastugugur85@gmail.com>
2026-03-17 23:58:47 +0300

    fix attempt 3: inconsistent button and field

--- src/components/savewin.jsx
+++ src/components/savewin.jsx
@@ -6,9 +6,9 @@ const SaveWin = ({addWin}) => {
     const [currentWin, setCurrentWin] = useState("")
 
 
-    return <div>
+    return <div id="save-win-con">
 
-        <input id="main-text-field"value={currentWin} required type="text" 
+        <input value={currentWin} required type="text" 
         onChange={(e) => setCurrentWin(e.target.value)}
         onKeyDown={(e) => {
         if (e.key === "Enter" && currentWin !== "") {
@@ -18,7 +18,7 @@ const SaveWin = ({addWin}) => {
         }}
         placeholder="Your win goes here. Then hit Enter!" />
 
-        <button id="submit-button" onClick={(e) => 
+        <button onClick={(e) => 
             {if (currentWin !== "")
             {addWin(currentWin)
              setCurrentWin("")   
